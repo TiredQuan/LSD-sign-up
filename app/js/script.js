9b292014bf0be5098d13968157ad80f2f8976c35
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-08-16 14:53:05 +0700

    EASY

--- app/js/script.js
+++ app/js/script.js
@@ -8,10 +8,10 @@ const tos = document.getElementById('TOS')
 
 
 signUpForm.addEventListener('submit', e =>{
-    e.preventDefault() //This stops the submit process entirely, we only use this for testing purposes
+    e.preventDefault()
 
     checkValidation()
-    //We will put a function name in here after we are one 
+    //We will put a function name in here after we are done
 })
 
 checkValidation = () => {
@@ -21,4 +21,39 @@ checkValidation = () => {
     const passwordVal = password.value.trim()
     const password2Val = password2.value.trim()
     const tosVal = tos.checked
+
+    let submitPnts = 0
+
+    const errorArr = document.getElementsByClassName('error')
+
+    //name
+    if(fullNameVal === ''){
+        errorArr[0].classList.add('error-show')
+    } else {errorArr[0].classList.remove('error-show'); submitPnts++}
+    //email
+    if(!checkEmail(emailVal)){
+        errorArr[1].classList.add('error-show')
+    } else {errorArr[1].classList.remove('error-show'); submitPnts++}
+    //phone
+    if(phoneNumVal === ''){
+        errorArr[2].classList.add('error-show')
+    } else {errorArr[2].classList.remove('error-show'); submitPnts++}
+    //pass
+    if(passwordVal.length < 6){
+        errorArr[3].classList.add('error-show')
+    } else {errorArr[3].classList.remove('error-show'); submitPnts++}
+    //pass2
+    if(password2Val !== passwordVal || password2Val ===''){
+    errorArr[4].classList.add('error-show')
+    } else {
+        errorArr[4].classList.remove('error-show'); submitPnts++
+    }
+    //name
+    if(!tosVal){
+        errorArr[5].classList.add('error-show')
+    } else {errorArr[5].classList.remove('error-show'); submitPnts++}
+    if(submitPnts == 6) signUpForm.submit()
+}
+function checkEmail(x) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(x);
 }
